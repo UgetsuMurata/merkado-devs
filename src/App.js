827776e18a65a6659d57cfcb97533035e9a72713
MurataUgetsu <yanezakai@gmail.com>
2024-11-01 00:10:31 +0800

    try again fix?

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <BrowserRouter>
           <Routes>
             <Route path="/" element={<Bugs/>} />
+            <Route path="/index.html" element={<Bugs/>} />
             <Route path="/version-control" element={<VersionControl/>} />
           </Routes>
       </BrowserRouter>
